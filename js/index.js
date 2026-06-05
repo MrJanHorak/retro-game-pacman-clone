6a0d7826690983ab388c38455883f3ff12945c62
MrJanHorak <janny_b_goode@hotmail.com>
2026-06-05 16:49:15 +0200

    begin basic screen layout

--- js/index.js
+++ js/index.js
@@ -9,14 +9,46 @@
 
 
 // initial variables for game state
-let score, high-score, lives, gameOver, gameStarted, level, player
+let score, highScore, lives, gameOver, gameStarted, level, player, bonus
 
 // Cached DOM elements
-
+const body = document.querySelector('body')
 
 
 
 // create DOM elements for game play
+const topInfoBar = document.createElement('div')
+topInfoBar.classList.add('top-info-bar')
+body.appendChild(topInfoBar)
+
+const scoreEl = document.createElement('div')
+scoreEl.classList.add('score')
+scoreEl.textContent = `Score: ${score}`
+topInfoBar.appendChild(scoreEl)
+
+const highScoreEl = document.createElement('div')
+highScoreEl.classList.add('high-score')
+highScoreEl.textContent = `High Score: ${highScore}`
+topInfoBar.appendChild(highScoreEl)
+
+const gameCanvas = document.createElement('canvas')
+gameCanvas.classList.add('game-canvas')
+body.appendChild(gameCanvas)
+
+const bottomInfoBar = document.createElement('div')
+bottomInfoBar.classList.add('bottom-info-bar')
+body.appendChild(bottomInfoBar)
+
+const livesEl = document.createElement('div')
+livesEl.classList.add('lives')
+livesEl.textContent = `Lives: ${lives}`
+bottomInfoBar.appendChild(livesEl)
+
+const bonusEl = document.createElement('div')
+bonusEl.classList.add('bonus')
+bonusEl.textContent = `Bonus: ${bonus}`
+bottomInfoBar.appendChild(bonusEl)
+
 
 // event listeners (for keyboard input, buttons, etc.)
 
